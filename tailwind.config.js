82831f69fa336d484136631b34b2d049364b0316
m_logicgate <miraj.logicseed@gmail.com>
2024-06-20 12:42:31 +0545

    Added brand color accents

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,26 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: { "50": "#eff6ff", "100": "#dbeafe", "200": "#bfdbfe", "300": "#93c5fd", "400": "#60a5fa", "500": "#3b82f6", "600": "#2563eb", "700": "#1d4ed8", "800": "#1e40af", "900": "#1e3a8a" }
-      }
+        primary: {
+          50: "#eff6ff",
+          100: "#dbeafe",
+          200: "#bfdbfe",
+          300: "#93c5fd",
+          400: "#60a5fa",
+          500: "#3b82f6",
+          600: "#2563eb",
+          700: "#1d4ed8",
+          800: "#1e40af",
+          900: "#1e3a8a",
+        },
+        logic: {
+          pink: "#ff6969",
+          red: "#c80036",
+          navy: "#0c1844",
+          cream: "#fff5e1",
+        },
+      },
     },
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
-}
+  plugins: [require("flowbite/plugin")],
+};
